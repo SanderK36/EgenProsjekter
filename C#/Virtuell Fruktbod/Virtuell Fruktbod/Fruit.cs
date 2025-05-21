7053464375b9ea63a278f994e1a11aded8713476
SanderK36 <sander.k606@gmail.com>
2025-05-21 15:02:18 +0200

    21.05

---
+++ C#/Virtuell Fruktbod/Virtuell Fruktbod/Fruit.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Virtuell_Fruktbod
+{
+    internal class Fruit
+    {
+         private string Name {get; set; }
+         private double PriceKg { get; set; }
+         private int KiloInStorage { get; set; }
+
+         public Fruit(string name, double priceKg, int kiloInStorage)
+         {
+             Name = name;
+             PriceKg = priceKg;
+            KiloInStorage = kiloInStorage;
+         }
+
+         public void ShowInfo()
+         {
+            Console.WriteLine($"{Name}");
+            Console.WriteLine($"Pris pr kilo: {PriceKg}");
+            Console.WriteLine($"Kilo på lager: {KiloInStorage}");
+        }
+
+         public void AddStock(int kilos)
+        {
+            if (kilos > 0)
+            {
+                KiloInStorage += kilos;
+                Console.WriteLine($"{kilos} kilo lagt til. {Name} har nå {KiloInStorage} kilo på lager");
+            }
+            else
+            {
+                Console.WriteLine("Kan ikke legge til 0 eller negative kilo!");
+            }
+        }
+
+        public void SellFruit(int kilos)
+        {
+            if (kilos > 0)
+            {
+                if (kilos <= KiloInStorage)
+                {
+                    KiloInStorage -= kilos;
+                    Console.WriteLine($"{kilos} kilo av {Name} solgt. {KiloInStorage} kilo igjen på lager.");
+                }
+                else
+                {
+                    Console.WriteLine($"Ikke nok {Name} på lager! Kun {KiloInStorage} kilo tilgjengelig.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Kan ikke selge 0 eller negative kilo!");
+            }
+        }
+
+        public string GetName()
+        {
+            return Name;
+        }
+    }
+}
---
+++ C#/Virtuell Fruktbod/Virtuell Fruktbod/Program.cs
@@ -0,0 +1,90 @@
+﻿using System.ComponentModel.Design;
+
+namespace Virtuell_Fruktbod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Fruit apple = new Fruit("Eple", 25.50, 100);
+            Fruit banana = new Fruit("Banan", 20.00, 50);
+            Fruit orange = new Fruit("Appelsin", 30.00, 75);
+            //Fruit pear = new Fruit("Pære", 22.00, 60);
+            //Fruit grape = new Fruit("Druer", 35.00, 40);
+            //Fruit watermelon = new Fruit("Vannmelon", 50.00, 20);
+            //Fruit mango = new Fruit("Mango", 45.00, 30);
+
+            while (true)
+            {
+                Console.WriteLine("Velg en frukt!");
+                Console.WriteLine("1: Eple");
+                Console.WriteLine("2: banan");
+                Console.WriteLine("3: Appelsin");
+                Console.WriteLine("4: Avslutt");
+                string choice = Console.ReadLine();
+                Fruit selectedFruit = null;
+                if(choice == "1")
+                    selectedFruit = apple;
+                else if (choice == "2")
+                    selectedFruit = banana;
+                else if (choice == "3")
+                    selectedFruit = orange;
+                else if (choice == "4")
+                {
+                   Console.WriteLine("Programmet avsluttes.");
+                   break;
+                }
+                else
+                {
+                    Console.WriteLine("Ugyldig valg, prøv igjen.");
+                    continue;
+                }
+                Console.Clear();
+                Console.WriteLine($"Hva vil du gjøre med {selectedFruit.GetName()}?");
+                Console.WriteLine("1: Legge til lager");
+                Console.WriteLine("2: Selg frukt");
+                Console.WriteLine("3: Vis info");
+                string action = Console.ReadLine();
+
+                if (action == "1")
+                {
+                    Console.Clear();
+                    Console.WriteLine($"Hvor mange kilo {selectedFruit.GetName()} vil du legge til?");
+                    if (int.TryParse(Console.ReadLine(), out int kilos))
+                    {
+                        selectedFruit.AddStock(kilos);
+                    }
+                    else
+                    {
+                        Console.WriteLine("ugyldig antall, skriv et tall.");
+                    }
+                }
+                else if (action == "2")
+                {
+                    Console.Clear();
+                    Console.WriteLine($"Hvor mange kilo {selectedFruit.GetName()} vil du selge?");
+                    if (int.TryParse(Console.ReadLine(), out int kilos))
+                    {
+                        selectedFruit.SellFruit(kilos);
+                    }
+                    else
+                    {
+                        Console.WriteLine("ugyldig antall, skriv et tall.");
+                    }
+                }
+                else if (action == "3")
+                {
+                    Console.Clear();
+                    Console.WriteLine($"Du har valgt: {selectedFruit.GetName()}");
+                    selectedFruit.ShowInfo();
+                }
+                else
+                {
+                    Console.WriteLine("Ugyldig valg, prøv igjen");
+                }
+                Console.WriteLine("Trykk på en tast for å fortsette...");
+                Console.ReadLine();
+            }
+        }
+    }
+}
