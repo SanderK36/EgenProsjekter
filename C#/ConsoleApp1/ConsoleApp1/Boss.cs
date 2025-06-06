e3d7fd08fc4b857526d56724ef1117de21054c1e
SanderK36 <sander.k606@gmail.com>
2025-06-06 14:55:59 +0200

    06.06

---
+++ C#/ConsoleApp1/ConsoleApp1/Boss.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WoW
+{
+    internal class Boss
+    {
+        public int BossHp { get; set; }
+        public int BossMaxHp { get; set; }
+        public int BossAttackPower { get; set; }
+        public string BossName { get; set; }
+        public int BossLevel { get; set; }
+
+        public Boss(int bossHp, int bossAttackPower, string bossName, int bossLevel)
+        {
+            BossHp = bossHp;
+            BossAttackPower = bossAttackPower;
+            BossName = bossName;
+            BossLevel = bossLevel;
+            BossMaxHp = bossHp;
+        }
+    }
+}
